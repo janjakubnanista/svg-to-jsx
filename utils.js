822e5d503b741a76909d236092b53c7967909baa
Ján Jakub Naništa <jan.jakub.nanista@gmail.com>
2016-01-06 21:15:56 +0100

    Added image to the list of valid SVG tags

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ var ALLOWED_HTML_ATTRIBUTES = 'accept acceptCharset accessKey action allowFullSc
 var ALLOWED_SVG_ATTRIBUTES = 'clipPath cx cy d dx dy fill fillOpacity fontFamily fontSize fx fy gradientTransform gradientUnits markerEnd markerMid markerStart offset opacity patternContentUnits patternUnits points preserveAspectRatio r rx ry spreadMethod stopColor stopOpacity stroke strokeDasharray strokeLinecap strokeOpacity strokeWidth textAnchor transform version viewBox x1 x2 x y1 y2 y xlink:actuate xlink:arcrole xlink:href xlink:role xlink:show xlink:title xlink:type xml:base xml:lang xml:space'.split(' ');
 
 var ALLOWED_ATTRIBUTES = ALLOWED_HTML_ATTRIBUTES.concat(ALLOWED_SVG_ATTRIBUTES);
-var ALLOWED_TAGS = 'circle clipPath defs ellipse g line linearGradient mask path pattern polygon polyline radialGradient rect stop svg text tspan'.toLowerCase().split(' ');
+var ALLOWED_TAGS = 'circle clipPath defs ellipse g image line linearGradient mask path pattern polygon polyline radialGradient rect stop svg text tspan'.toLowerCase().split(' ');
 
 exports.cssProperty = function(string) {
     var unprefixed = string.replace(/^-ms/, 'ms');
